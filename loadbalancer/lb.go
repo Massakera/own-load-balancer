99dc8f10a8c2682142c178acd5486e5eaad7537d
Massakera <enzomassaki@hotmail.com>
2024-02-05 13:35:47 -0300

    feature: adding health check

--- loadbalancer/lb.go
+++ loadbalancer/lb.go
@@ -2,25 +2,66 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"log"
 	"net"
 	"net/http"
 	"sync"
+	"time"
 )
 
 var backends = []string{"backend1:8080", "backend2:8080", "backend3:8080"}
 var currentServer int
 var mutex sync.Mutex
+var (
+	healthCheckURL    string
+	healthCheckPeriod int
+)
+var (
+	activeBackends []string
+)
+
+func checkBackendHealth(backend string) bool {
+	resp, err := http.Get("http://" + backend + healthCheckURL)
+	if err != nil || resp.StatusCode != 200 {
+		return false
+	}
+	return true
+}
+
+func updateActiveBackends() {
+	for {
+		mutex.Lock()
+		activeBackends = activeBackends[:0]
+		for _, backend := range backends {
+			if checkBackendHealth(backend) {
+				activeBackends = append(activeBackends, backend)
+			}
+		}
+		mutex.Unlock()
+		time.Sleep(time.Duration(healthCheckPeriod) * time.Second)
+	}
+}
 
 func getNextBackend() string {
 	mutex.Lock()
 	defer mutex.Unlock()
-	backend := backends[currentServer]
-	currentServer = (currentServer + 1) % len(backends)
+	if len(activeBackends) == 0 {
+		return ""
+	}
+	backend := activeBackends[currentServer%len(activeBackends)]
+	currentServer++
 	return backend
 }
 
 func main() {
+	// health check
+	flag.StringVar(&healthCheckURL, "health-check-url", "/health", "URL for health checking")
+	flag.IntVar(&healthCheckPeriod, "health-check-period", 10, "Health check period in seconds")
+	flag.Parse()
+	go updateActiveBackends()
+
+	// starts to listen
 	listenAddr := ":80"
 
 	listener, err := net.Listen("tcp", listenAddr)
